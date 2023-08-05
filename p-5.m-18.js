5f18df4faac1d085add86e6bceb7da5ccef1b871
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-08-06 01:07:20 +0530

    print hello world 5 times

---
+++ p-5.m-18.js
@@ -0,0 +1,20 @@
+
+// Display Hello World 5 times
+
+for(var i=1; i <= 5; i++){
+    console.log("Hello World")
+}
+
+/* ..........................................
+                  for practice
+.......................................... */
+
+var x=1;
+while(x<=5){
+    // console.log("hi");
+    console.log("test");
+    x++;
+}
+
+
+console.log(x,i)
