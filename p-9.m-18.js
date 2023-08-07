f9875946f9220064920a0cb9eacf1b48994882dc
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-08-07 10:47:36 +0530

    Display all the element of array by using while loop

---
+++ p-9.m-18.js
@@ -0,0 +1,10 @@
+/* create an array for all mobile phones.
+Display all the element of array by using while loop */
+
+const mobile = ["lg","vivo","mi","poco","samsung","lenovo"];
+var x=0;
+while(x < mobile.length){
+  var mobileName = mobile[x];
+  console.log(mobileName);
+  x++;
+}
