198c20e652f67206e30e73e752beaae1a4304f82
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-08-08 01:49:08 +0530

    5 hours = 300 minutes conversion

---
+++ p-17.m-19.js
@@ -0,0 +1,12 @@
+/* Write a function that will take hour as the input parameter and 
+will convert it into minutes and will return the result in minutes. */
+
+// ----------------------------------------------------------------------------
+
+function getHour(hour){
+    const getMinute = hour * 60;
+    return getMinute;
+}
+const hr = 5;
+const min = getHour(hr);
+console.log(hr + " hours = "+min+" minutes");
