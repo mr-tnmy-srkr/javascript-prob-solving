7d718a64c4754bb41882b504163d9ce8858b6825
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-08-11 19:15:47 +0530

    hours to minutes conversion

--- p-17.m-19.js
+++ p-17.m-19.js
@@ -9,6 +9,6 @@ function getHour(hour){
 }
 const hr = 5;
 const min = getHour(hr);
-console.log(hr + " hours = "+min+" minutes");
+console.log(hr + " hours = " + min + " minutes");
 
 // output : 5 hours = 300 minutes
