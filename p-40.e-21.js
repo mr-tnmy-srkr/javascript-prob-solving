988b52414c68734fbae4eeeeb8cedc8b2545a37f
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-08-12 01:31:50 +0530

    find second largest number from an array

---
+++ p-40.e-21.js
@@ -0,0 +1,22 @@
+/* find second largest number from an array */
+
+//  -------------------------------------------------------------------------------
+
+const numbers1 = [ 2, 45, 10, 15, 20, 25, 30, 35, 40, 5, -50 ];
+const numbers2 = [ 1 ];
+
+function secondLargest(numbs){
+    if(numbs.length < 2){
+return "Array should have at least two elements";
+    }
+numbs.sort(function(a,b){
+return b-a;
+})
+return numbs[1];
+}
+console.log(secondLargest(numbers1));
+console.log(secondLargest(numbers2));
+
+/* output : 
+40
+Array should have at least two elements */
