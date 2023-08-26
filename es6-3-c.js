668e466979199ac932fa54a62541f49bac9d37b2
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-08-26 23:01:08 +0530

    create an object by dot and bracket notation

---
+++ es6-3-c.js
@@ -0,0 +1,12 @@
+// create an object by dot and bracket notation
+
+// ..........................................................................
+
+let obj ={};
+obj.x = "hello";
+obj["y"]="world";
+const property = "phone";
+obj[property]=1258004;
+console.log(obj);
+
+// output : { x: 'hello', y: 'world', phone: 1258004 }
--- es6-3.js
+++
@@ -1,8 +0,0 @@
-
-
-let obj ={};
-obj.x = "hello";
-obj["y"]="world";
-const property = "phone";
-obj[property]=1258004;
-console.log(obj)
