2badce2e1e3190d14f53f8378d40153a69f9ab0f
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-08-27 15:52:43 +0530

    question uploaded

---
+++ es6-5-m.js
@@ -0,0 +1,22 @@
+// task -1: console log the secondary school location of Sophia 
+let data = {
+    Sophia: {
+        id: 33,
+        study: [
+            {
+                primary:
+                    [
+                        { school_name: "ABC primary school" },
+                        { location: "Peters burg" }
+                    ]
+            },
+            {
+                secondary:
+                    [
+                        { school_name: "ABC secondary school" },
+                        { location: "St Lorence" }
+                    ]
+            },
+        ]
+    }
+}
