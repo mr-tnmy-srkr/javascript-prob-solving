d575f463522410ccfe297211bf70f4cd7a78f688
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-08-27 15:54:06 +0530

    question only

---
+++ es6-7-m.js
@@ -0,0 +1,25 @@
+// task-3 : access and then show habluder adda
+// show output Beginner
+let data2 = {
+    data:
+        [
+            {
+                bookId: 1,
+                bookDetails: {
+                    name: "habluder adda",
+                    category: "XYZ",
+                    price: "20$",
+                },
+                bookCategory: "Basic",
+            },
+            {
+                bookId: 2,
+                bookDetails: {
+                    name: "gobluder adda",
+                    category: "ABC",
+                    price: "40$",
+                },
+                bookCategory: "Beginner",
+            }
+        ]
+}
