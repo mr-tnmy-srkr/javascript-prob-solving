17274c8f468ec3ca29d225e69cfc8755f731556f
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-09-03 14:58:25 +0530

    RegExp \n and arrow function

---
+++ es6-8-m.js
@@ -0,0 +1,25 @@
+// 1) Write an arrow function that will take 3 parameters,
+//  will multiply the parameters and will return the result.
+
+// 2) Write the following sentence in three lines and print the result:
+//  I am a web developer. I love to code. I love to eat biryani.
+
+// ...............................................................................
+// answer no 1.
+
+const multiply = (a,b,c) => {
+const multiple = a*b*c;
+return multiple;
+}
+console.log(multiply(5,4,3));               // 60
+
+// .................................................................................
+// answer no 2.
+
+const sentence = 'I am a web developer. I love to code. I love to eat biryani.';
+console.log('I am a web developer.\nI love to code.\nI love to eat biryani.');
+
+
+                                        //   I am a web developer.
+                                        //   I love to code.
+                                        //   I love to eat biryani.
