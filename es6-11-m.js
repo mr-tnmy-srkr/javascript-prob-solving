1e577bf83a31a26f9befcf87fcc3cc01153866ca
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-09-04 15:20:59 +0530

    Return the average of the sum of the squared elements (forEach)

--- es6-11-m.js
+++ es6-11-m.js
@@ -5,3 +5,17 @@ a) Square each array element
 b) Calculate the sum of the squared elements
 
 c) Return the average of the sum of the squared elements */
+
+// .......................................................................
+
+const x = (numbs) =>{
+    let sum = 0;
+numbs.forEach((x) => {
+    const square = x*x;
+sum += square;
+});
+const avg = sum / (numbs.length);
+return avg;
+}
+const numbs = [2,4,1,5,4,6,-0];
+console.log(x(numbs))                           // 14
