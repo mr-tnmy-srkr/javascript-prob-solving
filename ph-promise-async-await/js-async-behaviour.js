fd0ef5ce7f7cbcfb0e806f52ae7f80e33acd3f28
mr-tnmy-srkr <138756815+mr-tnmy-srkr@users.noreply.github.com>
2023-09-11 15:16:39 +0530

    Update js-async-behaviour.js

--- ph-promise-async-await/js-async-behaviour.js
+++ ph-promise-async-await/js-async-behaviour.js
@@ -1,8 +1,44 @@
 console.log(1);
 console.log(2);
+
+// async start
 setTimeout(() => {
     console.log('lazy logged')
-},4000);
+},2000);
+
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Fetch error:', error);
+  });
+
+
+  async function fetchData() {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts/2');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error('Fetch error:', error);
+    }
+  }
+  
+  // Call the async function
+  fetchData();
+  
+// async end
+
 doSomething();
 console.log(4);
 console.log(5);
@@ -20,4 +56,24 @@ function doSomething(){
 4
 5
 6
+
+{
+  userId: 1,
+  id: 1,
+  title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+  body: 'quia et suscipit\n' +
+    'suscipit recusandae consequuntur expedita et cum\n' +
+    'reprehenderit molestiae ut ut quas totam\n' +
+    'nostrum rerum est autem sunt rem eveniet architecto'
+}
+{
+  userId: 1,
+  id: 2,
+  title: 'qui est esse',
+  body: 'est rerum tempore vitae\n' +
+    'sequi sint nihil reprehenderit dolor beatae ea dolores neque\n' +
+    'fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\n' +
+    'qui aperiam non debitis possimus qui neque nisi nulla'
+}
+
 lazy logged */
