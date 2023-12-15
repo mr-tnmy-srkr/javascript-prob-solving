76f36223025d4ad8e32ea2dfdcc0228de57b7ea0
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-12-15 23:52:12 +0530

    another method of fibonacci series by recursive

--- interview/fibonacci.js
+++ interview/fibonacci.js
@@ -1,29 +1,37 @@
 // How do get nth Fibonacci number?
 
-function fibonacci(n){
-    var fibo = [0, 1];
-    
-    if (n <= 2) return 1;
-  
-    for (var i = 2; i <=n; i++ ){
-     fibo[i] = fibo[i-1]+fibo[i-2];
-     console.log(fibo[i]); 
-    }
-  
-   return fibo[n];
-  } 
-  
-console.log( fibonacci(12));  //Answer : 144
+function fibonacci(n) {
+  var fibo = [0, 1];
 
+  if (n <= 2) return 1;
 
-                              // 1
-                              // 2
-                              // 3
-                              // 5
-                              // 8
-                              // 13
-                              // 21
-                              // 34
-                              // 55
-                              // 89
-                              // 144 
+  for (var i = 2; i <= n; i++) {
+    fibo[i] = fibo[i - 1] + fibo[i - 2];
+    // console.log(fibo[i]);
+  }
+
+  return fibo[n];
+}
+
+console.log(fibonacci(12)); //Answer : 144
+
+// 1
+// 2
+// 3
+// 5
+// 8
+// 13
+// 21
+// 34
+// 55
+// 89
+// 144
+
+//   another method by recursive?
+
+function fibonacci2(m) {
+  if (m <= 1) return m;
+  else return fibonacci(m - 1) + fibonacci(m - 2);
+}
+
+console.log(fibonacci2(12)); //144
