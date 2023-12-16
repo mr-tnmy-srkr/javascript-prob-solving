9fc8aa3cd1f756effabb2fda36e4caefad62a2ea
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-12-16 22:14:53 +0530

    greatest common divisor of two numbers by  recursive function

--- interview/GreatestCommonDivisor.js
+++ interview/GreatestCommonDivisor.js
@@ -3,10 +3,10 @@
 
 function greatestCommonDivisor(a, b){
     var divisor = 2, 
-        greatestDivisor = 1;
+        greatestDivisor = 1; 
   
     //if u pass a -ve number this will not work. fix it dude!!
-    if (a < 2 || b < 2)
+     if (a < 2 || b < 2)
        return 1;
     
     while(a >= divisor && b >= divisor){
@@ -17,7 +17,19 @@ function greatestCommonDivisor(a, b){
     }
     return greatestDivisor;
   }
-  
+
+//    or by recursive function
+
+  function greatestCommonDivisor(a, b){
+    if(b == 0)
+      return a;
+    else 
+      return greatestCommonDivisor(b, a%b);
+ }
+     
+
   console.log(greatestCommonDivisor(14, 21))     // 7
    
   console.log(greatestCommonDivisor(69, 169))    // 1
+
+ 
