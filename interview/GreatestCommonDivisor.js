027aae6feab774b64da2dacf673dd5a325d9e1c8
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-12-16 22:07:37 +0530

    greatest common divisor of two numbers

---
+++ interview/GreatestCommonDivisor.js
@@ -0,0 +1,23 @@
+// How would you find the greatest common divisor of two numbers?
+
+
+function greatestCommonDivisor(a, b){
+    var divisor = 2, 
+        greatestDivisor = 1;
+  
+    //if u pass a -ve number this will not work. fix it dude!!
+    if (a < 2 || b < 2)
+       return 1;
+    
+    while(a >= divisor && b >= divisor){
+     if(a %divisor == 0 && b% divisor ==0){
+        greatestDivisor = divisor;      
+      }
+     divisor++;
+    }
+    return greatestDivisor;
+  }
+  
+  console.log(greatestCommonDivisor(14, 21))     // 7
+   
+  console.log(greatestCommonDivisor(69, 169))    // 1
