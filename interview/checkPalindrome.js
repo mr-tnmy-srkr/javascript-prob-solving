2234dd955bb1e7ad078eebbf2a8995f682775416
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2024-01-01 21:24:22 +0530

    check palindrome

---
+++ interview/checkPalindrome.js
@@ -0,0 +1,16 @@
+//  How will you verify a word as palindrome?
+
+// hints: if you reverse a word and it becomes same as the previous word, it is called palindrome.
+
+function isPalindrome(str){
+    var i, len = str.length;
+    for(i =0; i<len/2; i++){
+      if (str[i]!== str[len -1 -i])
+         return false;
+    }
+    return true;
+  }
+
+  console.log(isPalindrome('madam'))  // true
+
+  console.log(isPalindrome('toyota'))  //false
