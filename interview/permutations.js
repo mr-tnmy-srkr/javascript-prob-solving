5c3c8ca1dc8a84016fd27b9955ef2706497f75bd
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2024-01-03 23:42:26 +0530

    Permutations

---
+++ interview/permutations.js
@@ -0,0 +1,31 @@
+//  How would you create all permutation of a string?
+
+function permutations(str){
+    var arr = str.split(''),
+        len = arr.length, 
+        perms = [],
+        rest,
+        picked,
+        restPerms,
+        next;
+    
+        if (len == 0)
+            return [str];
+    
+        for (var i=0; i<len; i++)
+        {
+            rest = Object.create(arr);
+            picked = rest.splice(i, 1);
+    
+            restPerms = permutations(rest.join(''));
+    
+           for (var j=0, jLen = restPerms.length; j< jLen; j++)
+           {
+               next = picked.concat(restPerms[j]);
+               perms.push(next.join(''));
+           }
+        }
+       return perms;
+    }
+
+    console.log(permutations("abc")); //[ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
